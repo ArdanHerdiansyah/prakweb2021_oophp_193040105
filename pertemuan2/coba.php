deb3f5b91425eb755fdce0576c9b92c3bc38ade9
ArdanHerdiansyah <58681058+ArdanHerdiansyah@users.noreply.github.com>
2021-09-17 10:19:55 +0700

    Menyelesaikan video dari pertemuan 2 (objek dan class)

---
+++ pertemuan2/coba.php
@@ -0,0 +1,28 @@
+<?php 
+
+/*
+Ardan Herdiansyah
+193040105
+https://github.com/ArdanHerdiansyah/pw2021_193040105.git
+Pertemuan 2 - 17 September 2021
+Mempelajari Class dan Obejct
+*/
+
+ ?>
+
+ <?php 
+
+class Coba{
+
+
+}
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+  ?>
